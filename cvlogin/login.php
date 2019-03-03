82c2c5def390f525122788870a29458ddd2797e7
amya9 <32553026+amya9@users.noreply.github.com>
2019-03-03 17:51:39 +0530

    Add files via upload

---
+++ cvlogin/login.php
@@ -0,0 +1,29 @@
+<?php 
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and password = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+   
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
---
+++ cvlogin/welcome.php
@@ -0,0 +1,15 @@
+<?php
+   include('login.html');
+?>
+<html">
+   
+   <head>
+      <title>Welcome </title>
+   </head>
+   
+   <body>
+      <h1>Welcome <?php echo $login_session; ?></h1> 
+
+   </body>
+   
+</html>
